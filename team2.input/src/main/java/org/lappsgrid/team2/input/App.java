f40d2cf842d5b8ea72cae46eed0a5cfa40f41eea
jingsss <jsun2@andrew.cmu.edu>
2017-03-23 09:28:39 -0400

    first commit

---
+++ team2.input/src/main/java/org/lappsgrid/team2/input/App.java
@@ -0,0 +1,13 @@
+package org.lappsgrid.team2.input;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
