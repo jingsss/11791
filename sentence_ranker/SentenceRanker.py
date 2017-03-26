8174c9cd84cf88355da4ab6f7bd06d3cf4792f2a
TengZhong <teng.zhong@outlook.com>
2017-03-26 15:49:21 -0400

    update sentence ranker

--- sentence_ranker/SentenceRanker.py
+++ sentence_ranker/SentenceRanker.py
@@ -5,3 +5,9 @@ class SentenceRanker():
         :param token_view: json file format of token view
         """
         self.sentence_view, self.token_view = sentence_view, token_view
+
+    def rank_by_Jaccard(self):
+        pass
+
+    def rank_by_BM25(self):
+        pass
