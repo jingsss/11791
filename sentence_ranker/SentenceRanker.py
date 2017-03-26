d0dbd24e8d49da7c080e28a3102da7edd5b1371a
TengZhong <teng.zhong@outlook.com>
2017-03-26 15:44:29 -0400

    update sentence ranker

--- sentence_ranker/SentenceRanker.py
+++ sentence_ranker/SentenceRanker.py
@@ -1,3 +1,7 @@
 class SentenceRanker():
     def __init__(self, sentence_view, token_view):
+        """
+        :param sentence_view: json file format of sentence view
+        :param token_view: json file format of token view
+        """
         self.sentence_view, self.token_view = sentence_view, token_view
