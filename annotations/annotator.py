b17de683ab65fd327e336b57da3224b831d2f90a
jing sun <jing@jings-Air.wv.cc.cmu.edu>
2017-04-01 15:55:10 -0400

    annotator

--- annotations/annotator.py
+++ annotations/annotator.py
@@ -7,8 +7,8 @@ import unicodedata
 #nltk.download("words")
 #nltk.download('punkt')
 #nltk.download('averaged_perceptron_tagger')
-classifier = '../stanford/classifiers/english.muc.7class.distsim.crf.ser.gz'
-jar = '../stanford/stanford-ner.jar'
+classifier = 'stanford/classifiers/english.muc.7class.distsim.crf.ser.gz'
+jar = 'stanford/stanford-ner.jar'
 st = StanfordNERTagger(classifier,jar)
 # 
 
@@ -171,7 +171,7 @@ def create_annotations(sentence):
     final_ans['is_num'] = hasNumbers(new_tok)
 
     return final_ans
-
-S ="Jack Brown studies at Stony Brook University in New York since 1999 with 90% percentile at 5:00 pm in the evening in Oxford University. He is a student"
-print create_annotations(S)
+#
+#S ="To whom did the Virgin Mary allegedly appear in 1858 in Lourdes France"
+#print create_annotations(S)
 
