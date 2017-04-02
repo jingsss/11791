0bc77b9784889302742706e6f09915c5869fe2ad
nyy86 <nyy86@yahoo.com.tw>
2017-04-01 21:20:23 -0400

    modified readme

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ currrent classifier:
 11791/question-classification/question-classifier/question_classifier.py
 
 how to run:
-python ./question_classifier.py  ./data/train ./data/test/
+
+python ./question-classification/question-classifier/question_classifier.py ./question-classification/question-classifier/data/train   ./question-classification/question-classifier/data/test/
 
 
 Question type and testing result:
--- question-classification/question-classifier/question_classifier.py
+++ question-classification/question-classifier/question_classifier.py
@@ -105,35 +105,15 @@ def question_classifier_predict(docs_test1):
 
 
 def question_classify(inputs):
-    res_cat = ['ABBR','DESC','ENTY','HUM','LOC','NUM']
-    #res = urllib2.urlopen("http://127.0.0.1:8888/pipeline?row=" + row_num).read()
-#    print "question classifier input :  \n"
-    #print inputs
-    #print json.dumps(inputs, indent=4, sort_keys=True)
-    #ress =  json.loads(str(inputs))
-    #for item in ress["payload"]["views"]["annotations"]:
-    #    print item
-
-
     test_set = list()
     for view in  inputs['payload']['views']:
         for anno in  view['annotations']:
             if anno['id'] == "Q":
-#                print anno['features']['target']
-
                 test_set.append(anno['features']['target'])
-#                print test_set
-
-
                 y_predicted = question_classifier_predict(test_set)
-#                print y_predicted
                 anno['features']['question_type'] = str(y_predicted)
                 del test_set[:]
-    #print json.dumps(inputs, indent=4, sort_keys=True)
-    #print y_predicted
     return inputs
-
-#def question_classify_main(inputs):
 
 if __name__ == "__main__":
     print "question classifier input :  \n"
@@ -143,13 +123,9 @@ if __name__ == "__main__":
     y_test = list()
     docs_test = list()
 
-    #questions_data_folder = sys.argv[1]
-    #dataset = load_files(questions_data_folder, shuffle=False)
-    #print("n  training samples: %d" % len(dataset.data))
     fname = "./question-classification/question-classifier/data/train_5500.label.txt"
     with codecs.open(fname, encoding='utf-8', errors='ignore') as f:
         content = f.readlines()
-    #content = [x.strip() for x in content]
     for line in content:
         splited = line.split(' ', 1 )
         PrimaryTag = splited[0].split(':',1)[0]
@@ -181,21 +157,6 @@ if __name__ == "__main__":
     print docs_train
     print y_test
     print y_train
-    # you may also want to remove whitespace characters like `\n` at the end of each line
-    #content = [x.strip() for x in content]
-
-    # split the dataset in training and test set:
-    #docs_train, _, y_train, _ = train_test_split(
-    #    dataset.data, dataset.target, test_size=0.0, random_state=None)
-
-    # the testing data folder must be passed as first argument
-    #questions_data_folder1 = sys.argv[2]
-    #dataset1 = load_files(questions_data_folder1, shuffle=False)
-    #print("n testing  samples: %d" % len(dataset1.data))
-
-    # split the dataset in training and test set:
-    #_, docs_test1, _, y_test = train_test_split(
-    #    dataset1.data, dataset1.target, test_size=499, random_state=None)
 
     y_predicted = question_classifier_train(docs_train, y_train, docs_test)
     print "+++++++++++++++++++++++++++"
