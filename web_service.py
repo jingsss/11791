de511def407b409363939cefc03c7cad2d48aa9c
nyy86 <nyy86@yahoo.com.tw>
2017-04-23 10:13:34 -0400

    modified

--- web_service.py
+++ web_service.py
@@ -242,11 +242,11 @@ def answer_extractor():
 						info = info[question_type]
 						q = question.lower()
 						info = [i for i in info if i.lower() not in q]
-                                        elif question_type in entity_info:
-						entity_info = entity_info[question_type]
-						#info = entity_info.lower()
-						q = question.lower()
-						info = [i for i in info if i.lower() not in q]
+                                        #elif question_type in entity_info:
+					#	entity_info = entity_info[question_type]
+					#	#info = entity_info.lower()
+					#	q = question.lower()
+					#	info = [i for i in info if i.lower() not in q]
                                         else:
 						info = []
 					if len(info) > 0:
